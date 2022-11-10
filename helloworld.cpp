e68f5e8be7b5a24e4327c45a7c45421b49eeb4e1
VeronikaKlimenko <V.Klymenko_FIT_2_22_B_d@knute.edu.ua>
2022-11-10 19:18:05 +0200

    Update helloworld.cpp
    second version

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+
 using namespace std;
 int main(){
     int age;
     cout<<"What's ur age? ";
     cin>>age;
     cout<<"Your age is " <<age;
-    
+    //some comments by Veronika
     return 0;
 }
